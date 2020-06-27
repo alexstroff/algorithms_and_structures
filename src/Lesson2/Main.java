328eb2a4d516c3c2c4b7dc57b0cf4e09ce873b20
Alexei <alexei.strelnikov@gmail.com>
2020-06-27 12:34:57 +0300

    Lesson_2

---
+++ src/Lesson2/Main.java
@@ -0,0 +1,9 @@
+package Lesson2;
+
+public class Main {
+    public static void main(String[] args) {
+        Array array = new Array(1, 2, 3, 4, 5);
+        array.set(0, 5);
+        System.out.println(array.toString());
+    }
+}
