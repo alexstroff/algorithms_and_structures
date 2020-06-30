fcf8a482451f6dc0534ed516a63e2a2b974f3c4e
Alexei <alexei.strelnikov@gmail.com>
2020-06-30 12:01:01 +0300

    Lesson_2_HomeTask

--- src/Lesson2/Array.java
+++ src/Lesson2/Array.java
@@ -64,6 +64,114 @@ public class Array {
     // delete(index);
     // deleteAll();
 
+    /*
+По моему написанный алгоритм намного медленнее пузырьковой сортировки.
+Более приличный вариант в голову не пришел...
+ */
+    public void sortCount(){
+        long start = System.currentTimeMillis();
+        int max = arr[0];
+        int min = arr[0];
+        int x = 0;
+        int[] tempArr = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            if(arr[i] > max) max = arr[i];
+            if (arr[i] < min) min = arr[i];
+        }
+
+        int counterPlus[] = new int[max+1];
+        int counterMinus[] = new int[-min+1];
+
+        for (int i = 0; i < size; i++) {
+            if(arr[i] >= 0){
+                counterPlus[arr[i]]++;
+            }else {
+                counterMinus[-arr[i]]++;
+            }
+        }
+        for (int i = counterMinus.length - 1; i > 0; i--) {
+            if (counterMinus[i] != 0){
+                for (int j = 0 + x; j < counterMinus[i] + x; j++) {
+                    tempArr[j] = -i;
+                }
+                x += counterMinus[i];
+            }
+        }
+        for (int i = 0; i < counterPlus.length; i++) {
+            if (counterPlus[i] != 0){
+                for (int j = 0 + x; j < counterPlus[i] + x; j++) {
+                    tempArr[j] = i;
+                }
+                x += counterPlus[i];
+            }
+        }
+        System.arraycopy(tempArr, 0, arr, 0, size);
+        isSorted = true;
+        System.out.println(System.currentTimeMillis() - start);
+    }
+
+
+    public void insert(int index, int value){
+        if (index >= size || index < 0)
+            throw new ArrayIndexOutOfBoundsException(index);
+        if (size >= arr.length) {
+            increaseCapacity();
+        }
+        int[] temp1 = new int[index];
+        int[] temp2 = new int[size-index];
+        System.arraycopy(arr, 0, temp1, 0, index);
+        System.arraycopy(arr, index, temp2, 0, size - index);
+        System.arraycopy(temp1, 0, arr, 0, index);
+        arr[index] = value;
+        System.arraycopy(temp2, 0, arr, index + 1, size++ - index);
+        isSorted = false;
+    }
+
+    public void deleteVal(int value){
+        int[] tmp = new int[size];
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            if (arr[i] != value) {
+                tmp[count] = arr[i];
+                count++;
+            }
+        }
+        if (size == count){
+            throw new RuntimeException("No such elemen in array");
+        }else {
+            size = count;
+            System.arraycopy(tmp, 0, arr, 0, size);
+        }
+
+    }
+
+    public int deleteIndex(int index){
+        if (index >= size || index < 0)
+            throw new ArrayIndexOutOfBoundsException(index);
+        int num = arr[index];
+        if (index == 0){
+            for (int i = 1; i < size; i++) {
+                arr[i - 1] = arr[i];
+            }
+            size--;
+        }else {
+            int[] tmp1 = new int[index];
+            int[] tmp2 = new int[size - index - 1];
+            System.arraycopy(arr, 0, tmp1, 0, index);
+            System.arraycopy(arr, index + 1, tmp2, 0, size - index - 1);
+            System.arraycopy(tmp1, 0, arr, 0, index - 1);
+            System.arraycopy(tmp2, 0, arr, index, size - index - 1);
+            size--;
+        }
+        return num;
+    }
+
+    public void deleteAll(){
+        size = 0;
+    }
+
+
 
 
     @Override
@@ -118,6 +226,7 @@ public class Array {
     }
 
     public void sortBubble() {
+        long start = System.currentTimeMillis();
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size - 1; j++) {
                 if (arr[j] > arr[j + 1])
@@ -125,6 +234,7 @@ public class Array {
             }
         }
         isSorted = true;
+        System.out.println(System.currentTimeMillis() - start);
     }
 
     public void sortSelect() {
--- src/Lesson2/Main.java
+++ src/Lesson2/Main.java
@@ -2,8 +2,19 @@ package Lesson2;
 
 public class Main {
     public static void main(String[] args) {
-        Array array = new Array(1, 2, 3, 4, 5);
-        array.set(0, 5);
+        Array array = new Array(5,1,4,1,3,2,1);
         System.out.println(array.toString());
+//        array.sortCount();
+//        array.sortBubble();
+//        array.sortInsert();
+//        array.sortSelect();
+//        array.insert(0,3);
+//        array.deleteVal(88);
+//        array.deleteIndex(0);
+//        array.deleteAll();
+        array.sortCount2();
+        System.out.println(array.toString());
+
+
     }
 }
