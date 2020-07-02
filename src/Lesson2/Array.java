cdfb239536f542f77d9c31d238aaf3f4627d7b79
Alexei <alexei.strelnikov@gmail.com>
2020-07-02 20:59:56 +0300

    Lesson_3

--- src/Lesson2/Array.java
+++ src/Lesson2/Array.java
@@ -6,11 +6,12 @@ public class Array {
     private boolean isSorted;
 
     private Array() {
-        this.isSorted = false;
+        isSorted = false;
     }
 
     public Array(int capacity) {
         this();
+        if (capacity == 0) capacity = 1;
         arr = new int[capacity];
         this.size = 0;
     }
@@ -21,175 +22,109 @@ public class Array {
         this.arr = args;
     }
 
-    public int get(int index) {
-        if (index >= size || index < 0)
-            throw new ArrayIndexOutOfBoundsException(index);
-        return arr[index];
-    }
-
-    public void set (int index, int value) {
-        if (index >= size || index < 0)
-            throw new ArrayIndexOutOfBoundsException(index);
-        arr[index] = value;
+    public Array(Array from) {
+        this.arr = new int[from.size];
+        this.size = from.size;
+        this.isSorted = from.isSorted;
+        for (int i = 0; i < from.size; i++)
+            arr[i] = from.get(i);
     }
 
     public int length() {
         return size;
     }
 
+    public int get(int index) {
+        if (index >= size)
+            throw new ArrayIndexOutOfBoundsException(index);
+        return arr[index];
+    }
+
+    public void set(int index, int value) {
+        if (index >= size)
+            throw new ArrayIndexOutOfBoundsException(index);
+        arr[index] = value;
+    }
+
+    boolean delete() { // last
+        if (size == 0) return false;
+        size--;
+        return true;
+    }
+
+    boolean delete(int index) { // by index
+        if (index >= size || index < 0)
+            throw new ArrayIndexOutOfBoundsException(index);
+        copyLeft(index);
+        size--;
+        return true;
+    }
+
+    private void copyLeft(int index) {
+        System.arraycopy(arr, index + 1, arr, index, size - index - 1);
+    }
+
+    private void copyRight(int index) {
+        System.arraycopy(arr, index, arr, index + 1, size - index - 1);
+    }
+
     private void increaseCapacity() {
         int[] temp = arr;
-        arr = new int[size * 2];
+        arr = new int[arr.length * 2];
         System.arraycopy(temp, 0, arr, 0, size);
     }
 
     public void append(int value) {
-        if (size >= arr.length) {
-            increaseCapacity();
-        }
+        checkCapacity();
         arr[size++] = value;
         isSorted = false;
     }
 
-    public int deleteLast() {
-        if (size == 0)
-            throw new ArrayIndexOutOfBoundsException(-1);
-
-        return arr[--size];
-    }
-
-    // homework
-    // insert(index, value);
-    // delete(val);
-    // delete(index);
-    // deleteAll();
-
-    /*
-По моему написанный алгоритм намного медленнее пузырьковой сортировки.
-Более приличный вариант в голову не пришел...
- */
-    public void sortCount(){
-        long start = System.currentTimeMillis();
-        int max = arr[0];
-        int min = arr[0];
-        int x = 0;
-        int[] tempArr = new int[size];
-
-        for (int i = 0; i < size; i++) {
-            if(arr[i] > max) max = arr[i];
-            if (arr[i] < min) min = arr[i];
-        }
-
-        int counterPlus[] = new int[max+1];
-        int counterMinus[] = new int[-min+1];
-
-        for (int i = 0; i < size; i++) {
-            if(arr[i] >= 0){
-                counterPlus[arr[i]]++;
-            }else {
-                counterMinus[-arr[i]]++;
-            }
-        }
-        for (int i = counterMinus.length - 1; i > 0; i--) {
-            if (counterMinus[i] != 0){
-                for (int j = 0 + x; j < counterMinus[i] + x; j++) {
-                    tempArr[j] = -i;
-                }
-                x += counterMinus[i];
-            }
-        }
-        for (int i = 0; i < counterPlus.length; i++) {
-            if (counterPlus[i] != 0){
-                for (int j = 0 + x; j < counterPlus[i] + x; j++) {
-                    tempArr[j] = i;
-                }
-                x += counterPlus[i];
-            }
-        }
-        System.arraycopy(tempArr, 0, arr, 0, size);
-        isSorted = true;
-        System.out.println(System.currentTimeMillis() - start);
-    }
-
-
-    public void insert(int index, int value){
-        if (index >= size || index < 0)
-            throw new ArrayIndexOutOfBoundsException(index);
+    private void checkCapacity() {
         if (size >= arr.length) {
             increaseCapacity();
         }
-        int[] temp1 = new int[index];
-        int[] temp2 = new int[size-index];
-        System.arraycopy(arr, 0, temp1, 0, index);
-        System.arraycopy(arr, index, temp2, 0, size - index);
-        System.arraycopy(temp1, 0, arr, 0, index);
-        arr[index] = value;
-        System.arraycopy(temp2, 0, arr, index + 1, size++ - index);
-        isSorted = false;
     }
 
-    public void deleteVal(int value){
-        int[] tmp = new int[size];
-        int count = 0;
-        for (int i = 0; i < size; i++) {
-            if (arr[i] != value) {
-                tmp[count] = arr[i];
-                count++;
-            }
-        }
-        if (size == count){
-            throw new RuntimeException("No such elemen in array");
-        }else {
-            size = count;
-            System.arraycopy(tmp, 0, arr, 0, size);
-        }
-
-    }
-
-    public int deleteIndex(int index){
-        if (index >= size || index < 0)
+    public void insert(int index, int value) {
+        if (index > size || index < 0) {
             throw new ArrayIndexOutOfBoundsException(index);
-        int num = arr[index];
-        if (index == 0){
-            for (int i = 1; i < size; i++) {
-                arr[i - 1] = arr[i];
-            }
-            size--;
-        }else {
-            int[] tmp1 = new int[index];
-            int[] tmp2 = new int[size - index - 1];
-            System.arraycopy(arr, 0, tmp1, 0, index);
-            System.arraycopy(arr, index + 1, tmp2, 0, size - index - 1);
-            System.arraycopy(tmp1, 0, arr, 0, index - 1);
-            System.arraycopy(tmp2, 0, arr, index, size - index - 1);
-            size--;
+        } else if (index == size) {
+            append(value);
+        } else {
+            checkCapacity();
+            copyRight(index);
+            arr[index] = value;
+            size++;
         }
-        return num;
     }
 
-    public void deleteAll(){
+//    boolean deleteAll(int value) { // by value
+//        boolean success = false;   //O(N^2)
+//        while (delete(value)) {
+//            success = true;
+//        }
+//        return success;
+//    }
+
+    boolean deleteAll(int value) {
+        boolean success = false;
+        for (int i = 0; i < size; i++) {
+            if (arr[i] == value) {
+                delete(i--);
+                success = true;
+            }
+        }
+        return success;
+    }
+
+    void deleteAll() { //clear array
         size = 0;
     }
 
-
-
-
-    @Override
-    public String toString() {
-        if (arr == null) return "null";
-        int iMax = size - 1;
-        if (iMax == -1) return "[]";
-
-        StringBuilder b = new StringBuilder();
-        b.append('[');
-        int i = 0;
-        while (true) {
-            b.append(arr[i]);
-            if (i == iMax)
-                return b.append(']').toString();
-            b.append(", ");
-            i++;
-        }
+    public void appendRandom(int elements, int range) {
+        for (int i = 0; i < elements; i++)
+            append((int) (Math.random() * range));
     }
 
     public int find(int value) {
@@ -202,16 +137,17 @@ public class Array {
 
     public boolean hasValue(int value) {
         if (!isSorted)
-            throw new RuntimeException("try the 'find' method");
-
+            throw new RuntimeException("Trying to search in unsorted array");
         int l = 0;
         int r = size;
         int m;
         while (l < r) {
-            m = (l + r) >> 1; // (l + r) / 2
+            // n >> k == n / 2 ^ k
+            m = (l + r) >> 1; // 8 = 00001000 >> 1 = 00000100 = 4
             if (value == arr[m])
                 return true;
-            else if (value < arr[m])
+            else
+            if (value < arr[m])
                 r = m;
             else
                 l = m + 1;
@@ -225,39 +161,127 @@ public class Array {
         arr[b] = temp;
     }
 
-    public void sortBubble() {
-        long start = System.currentTimeMillis();
+    public int sortBubble() {
+        int count = 0;
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size - 1; j++) {
+                count++;
                 if (arr[j] > arr[j + 1])
                     swap(j, j + 1);
             }
         }
         isSorted = true;
-        System.out.println(System.currentTimeMillis() - start);
+        return count;
     }
 
-    public void sortSelect() {
-        for (int flag = 0; flag < size; flag++) {
-            int cMin = flag;
-            for (int rem = flag + 1; rem < size; rem++)
-                if (arr[rem] < arr[cMin])
-                    cMin = rem;
-            swap(flag, cMin);
+    public int sortBubbleImproved() {
+        int count = 0;
+        boolean flag = false;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size - 1 - i; j++) {
+                count++;
+                if (arr[j] > arr[j + 1]) {
+                    swap(j, j + 1);
+                    flag = true;
+                }
+            }
+            if (!flag) break;
         }
         isSorted = true;
+        return count;
     }
 
-    public void sortInsert() {
-        for (int out = 0; out < size; out++) {
+    public int sortBubbleEvenOdd() {
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = (i % 2 != 0) ? 0 : 1; j < size - 1; j += 2) {
+                count++;
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr[j], arr[j + 1]);
+                }
+            }
+        }
+        return count;
+    }
+
+    public int sortSelect() {
+        int count = 0;
+        int f;
+        for (int i = 0; i < size; i++) {
+            f = i;
+            for (int j = i + 1; j < size; j++) {
+                count++;
+                if (arr[j] < arr[f])
+                    f = j;
+            }
+            swap(i, f);
+        }
+        isSorted = true;
+        return count;
+    }
+
+    public int sortInsert() {
+        int count = 0;
+        int in;
+        for (int out = 1; out < size; out++) {
             int temp = arr[out];
-            int in = out;
+            in = out;
             while (in > 0 && arr[in - 1] >= temp) {
+                count++;
                 arr[in] = arr[in - 1];
                 in--;
             }
             arr[in] = temp;
         }
         isSorted = true;
+        return count;
+    }
+
+    @Override
+    public String toString() {
+        if (size == 0) return "[]";
+        StringBuilder b = new StringBuilder();
+        b.append('[');
+        for (int i = 0; ; i++) {
+            b.append(arr[i]);
+            if (i == size - 1)
+                return b.append(']').toString();
+            b.append(", ");
+        }
+    }
+
+    int getMax() {
+        if (size == 0) throw new RuntimeException("Empty array");
+        if (size == 1) return arr[0];
+        int r = arr[0];
+        for (int i = 1; i < size; i++) {
+            if (r < arr[i])
+                r = arr[i];
+        }
+        return r;
+    }
+
+    int getMin() {
+        if (size == 0) throw new RuntimeException("Empty array");
+        if (size == 1) return arr[0];
+        int r = arr[0];
+        for (int i = 1; i < size; i++) {
+            if (r > arr[i])
+                r = arr[i];
+        }
+        return r;
+    }
+
+    void pigeon() {
+        int min = getMin();
+        int max = getMax();
+        int[] freq = new int[max - min + 1];
+        for (int i = 0; i < size; i++)
+            freq[arr[i] - min]++;
+
+        int arrIndex = 0;
+        for (int i = 0; i < freq.length; i++)
+            for (int elems = freq[i]; elems > 0; elems--)
+                arr[arrIndex++] = i + min;
     }
 }
